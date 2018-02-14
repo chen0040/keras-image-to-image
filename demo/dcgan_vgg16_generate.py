472fab2c8d948451d3fce18be8aacaf3af1dab15
xschen <xs0040@gmail.com>
2018-02-15 05:51:23 +0800

    fix issues with the generate script of dcgan

--- demo/dcgan_vgg16_generate.py
+++ demo/dcgan_vgg16_generate.py
@@ -1,9 +1,11 @@
+import PIL
+
 from keras_image_to_image.library.dcgan_vgg16 import DCGanWithVGG16
 from keras_image_to_image.library.utility.image_utils import img_from_normalized_img
 from keras_image_to_image.library.utility.img_directory_loader import load_image_path_pairs
 import numpy as np
 from random import shuffle
-
+from PIL import Image
 
 def main():
     seed = 42
@@ -14,6 +16,9 @@ def main():
     txt_dir_path = './data/pokemon/dest_img'
     model_dir_path = './models'
 
+    img_width = 32
+    img_height = 32
+
     image_label_pairs = load_image_path_pairs(img_dir_path, txt_dir_path)
 
     shuffle(image_label_pairs)
@@ -22,14 +27,14 @@ def main():
     gan.load_model(model_dir_path)
 
     for i in range(3):
-        image_label_pair = image_label_pairs[i]
-        normalized_image = image_label_pair[0]
-        text = image_label_pair[1]
+        image_path_pair = image_label_pairs[i]
+        src_image_path = image_path_pair[0]
+        dest_image_path = image_path_pair[1]
 
-        image = img_from_normalized_img(normalized_image)
+        image = Image.open(dest_image_path).img.resize((img_width, img_height), PIL.Image.ANTIALIAS)
         image.save('./data/outputs/' + DCGanWithVGG16.model_name + '-generated-' + str(i) + '-0.png')
         for j in range(3):
-            generated_image = gan.generate_image_from_text(text)
+            generated_image = gan.generate_image_from_image_file(src_image_path)
             generated_image.save('./data/outputs/' + DCGanWithVGG16.model_name + '-generated-' + str(i) + '-' + str(j) + '.png')
 
 
